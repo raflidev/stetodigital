d9c204ff9025f2299c491913fedc884c0e6c8fc6
Rafli Ramadhan <rafliramdhn@gmail.com>
2022-12-10 09:33:30 +0700

    Update PasienController.php

--- app/Http/Controllers/Pasiens/PasienController.php
+++ app/Http/Controllers/Pasiens/PasienController.php
@@ -44,7 +44,7 @@ class PasienController extends Controller
         return view('pasien.pasien.owncheck', compact('dataSignal'));
     }
     
-    public function result()
+    public function resultbest()
     {
         $activeUser = User::find(Auth::id());
         return view('pasien.pasien.result');
